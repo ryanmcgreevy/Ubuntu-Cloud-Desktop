88e8d743584d04a3de5bded3736eb9b102effe3d
Ryan McGreevy <ryanmcgreevy@gmail.com>
2025-03-28 14:32:23 -0400

    added outbound rules to security group

--- terraform/modules/services/single_instance/main.tf
+++ terraform/modules/services/single_instance/main.tf
@@ -49,6 +49,16 @@ resource "aws_security_group_rule" "allow_rdp" {
   cidr_blocks = local.all_ips
 }
 
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.sec_group.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
  resource "aws_key_pair" "deployer" {
    key_name   = "deployer-key"
    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOrmtRK7DeHDOJ4am6idvME6qM9B+sPMfitahtt/DDJ"
