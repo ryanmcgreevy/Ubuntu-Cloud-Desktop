76e787e22a31c3bf049201f5c6638fbd82a563c6
Ryan McGreevy <ryanmcgreevy@gmail.com>
2025-04-28 14:51:05 -0400

    turned key checking off for ansible because it was messing with connecting to multiple instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "single_instance" {
   source = "./modules/services/single_instance"
 
   tag           = "fresh-install"
-  ami_id        = ["ami-028883ffdf98347c9","ami-09499b73854f5459f"]
+  ami_id        = ["ami-02183c955d69e6bb5","ami-0ae8209b58af63f53"]
   instance_type = ["t3.medium","g5g.xlarge"]
   #ami_id        = "ami-0ec612e1caf93df8c"
   #instance_type = "t3.medium"
