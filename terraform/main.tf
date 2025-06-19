98ccab715b14a0c57c810d2264165a1c3fe821e5
Ryan McGreevy <ryanmcgreevy@gmail.com>
2025-06-19 17:51:42 -0400

    new tomcat version

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ module "single_instance" {
   source = "./modules/services/single_instance"
 
   tag           = "fresh-install"
-  ami_id        = ["ami-02183c955d69e6bb5","ami-0ae8209b58af63f53"]
-  instance_type = ["t3.medium","g5g.xlarge"]
+  ami_id        = ["ami-020cba7c55df1f615","ami-07041441b708acbd6"]
+  instance_type = ["g5.xlarge","g5g.xlarge"]
   #ami_id        = "ami-0ec612e1caf93df8c"
   #instance_type = "t3.medium"
 }
