aa5afdbc93abd5678ebae318f295555b364ce872
Ryan McGreevy <ryanmcgreevy@gmail.com>
2025-07-08 15:56:07 -0400

    removed apt cache time because it was causing issues with long installs. fixed issue in no gpu xorg using wrong identifier.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ module "single_instance" {
   source = "./modules/services/single_instance"
 
   tag           = "fresh-install"
-  ami_id        = ["ami-020cba7c55df1f615","ami-07041441b708acbd6"]
-  instance_type = ["g5.xlarge","g5g.xlarge"]
-  #ami_id        = "ami-0ec612e1caf93df8c"
-  #instance_type = "t3.medium"
+  #ami_id        = ["ami-020cba7c55df1f615","ami-07041441b708acbd6"]
+  #instance_type = ["g5.xlarge","g5g.xlarge"]
+  ami_id        = ["ami-0f140b8689f89b1cd"]
+  instance_type = ["g5g.xlarge"]
 }
