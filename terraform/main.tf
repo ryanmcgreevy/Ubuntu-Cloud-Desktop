d9dbbccc288d326bb4dd39c41e2b6d69630599e5
Ryan McGreevy <ryanmcgreevy@gmail.com>
2025-11-03 17:18:32 -0500

    changed rdp to use command module. changed docker to pull specific version and arch

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,8 @@ module "single_instance" {
   tag           = "fresh-install"
   # ami_id        = ["ami-0f30a0245a537000d", "ami-0a8c16a4360048851"]
   # instance_type = ["g5.xlarge","g5g.xlarge"]
-  ami_id        = ["ami-0f30a0245a537000d"]
-  instance_type = ["t3.medium"]
-  
+  # ami_id        = ["ami-0f30a0245a537000d"]
+  # instance_type = ["t3.medium"]
+  ami_id        = ["ami-0a8c16a4360048851"]
+  instance_type = ["t4g.medium"]
 }
