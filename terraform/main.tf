9b19bb4911524a38d25f1c8fd25d4aaff8cd5a06
Ryan McGreevy <ryanmcgreevy@gmail.com>
2026-05-01 15:42:18 -0400

    updates to ansible cfg to bring functionality up to date with latest release. docker.yaml now has a variable for arch to be able to switch beteeen amd64 and arm64 more easily

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,6 @@ module "single_instance" {
   # instance_type = ["g5.xlarge","g5g.xlarge"]
   # ami_id        = ["ami-0f30a0245a537000d"]
   # instance_type = ["t3.medium"]
-   ami_id        = ["ami-0a8c16a4360048851"]
-   instance_type = ["g5g.xlarge"]
+   ami_id        = ["ami-05cf1e9f73fbad2e2"]
+   instance_type = ["g5.xlarge"]
 }
